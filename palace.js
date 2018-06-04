904982b1aeb60da15407eb55e5aef9da45c59a08
Matthew DiBello <matthew.quickbeam@gmail.com>
2018-06-04 15:50:35 -0400

    Some nice abstraction to avoid duplicate code

--- palace.js
+++ palace.js
@@ -1,8 +1,8 @@
 cards = {"hands":[6,6,6,6],"face_up_three":[[{"value":"Eight","suit":"Diamonds"},{"value":"Six","suit":"Hearts"},{"value":"Ten","suit":"Hearts"}],[{"value":"Six","suit":"Spades"},{"value":"Nine","suit":"Diamonds"},{"value":"Queen","suit":"Spades"}],[{"value":"Eight","suit":"Clubs"},{"value":"Eight","suit":"Hearts"},{"value":"Four","suit":"Diamonds"}],[{"value":"Three","suit":"Clubs"},{"value":"Two","suit":"Spades"},{"value":"Seven","suit":"Clubs"}]],"face_down_three":[3,3,3,3],"top_card":{"value":"King","suit":"Spades"},"pile_size":1,"cleared_size":0}
 
 numPlayers = cards['hands'].length;
-playerName = 'Richard';
-playerId = 2;
+myName = 'Richard';
+myId = 2;
 setNum = 3;
 backNum = 1;
 
@@ -12,6 +12,7 @@ function update() {
 
     var parentElement = document.getElementById('body');
 
+
     /////////////////
     // TABLE CARDS //
     /////////////////
@@ -19,27 +20,13 @@ function update() {
     newHtml += '<div id="other-players">';
 
     for (i = 0; i < numPlayers; i++) {
- 
-        if (i != playerId) { // if player is not you
-
-            newHtml += '<div class="player" id="player-' + i + '">';
-            newHtml += '<h3>' + playerName + '</h3><p>' + cards['hands'][i] + ' cards in hand</p>';
-
-            for (j = 0; j < cards['face_up_three'][i].length; j++) {
-                newHtml += generateCardHtml(cards['face_up_three'][i][j], setNum, 60);
-            }
-
-            newHtml += '</br>';
-
-            for (j = 0; j < cards['face_down_three'][i]; j++) {
-                newHtml += '<img src="img/backs/back-' + backNum + '.svg" width="60px">';
-            }
-
-            newHtml += '</div>';
+        if (i != myId) { // if player is not you
+            newHtml += generateTableHtml(i, "Richard", myId, setNum, backNum, 60);
         }
     }
 
     newHtml += '</div>';
+
 
     ////////////////
     // CARD STACK //
@@ -49,28 +36,55 @@ function update() {
     newHtml += generateCardHtml(cards['top_card'], setNum, 150);
     newHtml += '</div>';
 
+
     //////////////////
     // PLAYER CARDS //
     //////////////////
 
     newHtml += '<div id="my-cards">';
 
+    // player's table cards
     newHtml += '<div class="my-table" id="player-' + playerId + '">';
-
-    // player face-up cards
-    // player face-down cards
-
+    newHtml += generateTableHtml(myId, myName, myId, setNum, backNum, 100);
     newHtml += '</div>';
 
-    newHtml += '<div class="hand-cards" id="player-' + playerId + '">';
 
     // player's hand cards
+    newHtml += '<div class="hand-cards" id="player-' + playerId + '">';
 
     newHtml += '</div>';
 
     newHtml += '</div>';
 
     document.getElementById('body').innerHTML = newHtml;
+}
+
+function generateTableHtml(playerId, playerName, myId, setNum, backNum, cardWidth) {
+
+    html = '<div class="player" id="player-' + playerId + '">';
+
+    if (playerId != myId) {
+        html += '<h3>' + playerName + '</h3><p>' + cards['hands'][playerId] + ' cards in hand</p>';
+    }
+
+    for (j = 0; j < cards['face_up_three'][playerId].length; j++) {
+        html += generateCardHtml(cards['face_up_three'][playerId][j], setNum, cardWidth);
+    }
+
+    html += '</br>';
+
+    for (j = 0; j < cards['face_down_three'][playerId]; j++) {
+        html += '<img src="img/backs/back-' + backNum + '.svg" width="' + cardWidth + 'px">';
+    }
+
+    html += '</div>';
+}
+
+function generateCardHtml(card, setNum, width) {
+    html  = '<img src="img/set-' + setNum + '/' + getCardName(card) + '.svg"';
+    html += ' title="' + card['value'].toLowerCase() + ' of ';
+    html += card['suit'].toLowerCase() + '" width="' + width + 'px">';
+    return html;
 }
 
 function getCardName(card) {
@@ -120,11 +134,4 @@ function getCardName(card) {
     }
 
     return cardName + card['suit'][0];
-}
-
-function generateCardHtml(card, setNum, width) {
-    html  = '<img src="img/set-' + setNum + '/' + getCardName(card) + '.svg"';
-    html += ' title="' + card['value'].toLowerCase() + ' of ';
-    html += card['suit'].toLowerCase() + '" width="' + width + 'px">';
-    return html;
 }
