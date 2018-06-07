a574cde18c499bc6d0b6c29f413b4dda7e8fbd19
Matthew DiBello <matthew.quickbeam@gmail.com>
2018-06-07 19:59:20 -0400

    Some basic message handling for lobbies

--- landing.js
+++ landing.js
@@ -1,6 +1,45 @@
-window.onload = update;
+playerToken = '';
+lobbyToken = '';
+
+var socket = new WebSocket("ws://dev.brick.codes:3012");
+
+var listLobbiesBlob = new Blob(
+    [JSON.stringify("ListLobbies")],
+    {type:'application/json'}
+);
+
+var newLobbyBlob = new Blob(
+    [JSON.stringify(
+        {
+            "NewLobby": {
+                "max_players" : 4,
+                "password"    : "",
+                "lobby_name"  : "brickhouse",
+                "player_name" : "matt"
+            }
+        }
+    )],
+    {type:'application/json'}
+);
+
+socket.addEventListener('open', function (event) {
+    socket.send(newLobbyBlob);
+});
+
+socket.addEventListener('message', function (event) {
+    var reader = new FileReader();
+    reader.addEventListener("loadend", function() {
+        playerToken = reader.result['player_id'];
+        lobbyToken  = reader.result['lobby_id'];
+    });
+    reader.readAsText(event.data);
+});
+
+document.onload = update();
 
 function update() {
+
+    console.log(document.querySelector("#lobbies"));
 
     var dataTable = new DataTable("#lobbies", {
         searchable: true,
@@ -13,5 +52,4 @@ function update() {
             info: 'Showing {start} to {end} of {rows} lobbies'
         }
     });
-
 }
