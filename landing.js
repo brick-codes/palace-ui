6062953dd1143e5b5a37b1fe2ebb50deaf0255e5
Matthew DiBello <matthew.quickbeam@gmail.com>
2018-06-06 21:41:53 -0400

    Some small progress on langing page

--- landing.js
+++ landing.js
@@ -3,7 +3,15 @@ window.onload = update;
 function update() {
 
     var dataTable = new DataTable("#lobbies", {
-        searchable: true
+        searchable: true,
+        perPageSelect: false,
+        firstLast: true,
+        sortable: true,
+        labels: {
+            placeholder: 'Search for a lobby',
+            noRows: 'No lobbies found',
+            info: 'Showing {start} to {end} of {rows} lobbies'
+        }
     });
 
 }
